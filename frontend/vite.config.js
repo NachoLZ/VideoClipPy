011c90a06b615c1361d690ee4be8cbcb4793786a
Ignacio Lizana <ignacio.lizanaa@sansano.usm.cl>
2025-11-01 16:17:04 +0100

    Vercel deployment json

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,21 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    proxy: {
-      '/upload-video-with-txt': 'http://localhost:5000',
-      '/upload-clip': 'http://localhost:5000',
-      '/process-video': 'http://localhost:5000',
-      '/download': 'http://localhost:5000',
-      '/list-clips': 'http://localhost:5000'
-    }
   },
   build: {
-    outDir: 'dist',
-    emptyOutDir: true
-  }
-})
-
+    outDir: "dist",
+    emptyOutDir: true,
+  },
+});
